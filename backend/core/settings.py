e7db8852d4deeaca438006d3a506e28d882d3aef
Samuel.Ssenyonga <samuel.senyonga@baisoftglobal.co.ug>
2026-03-18 21:08:29 +0300

    minor

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -151,7 +151,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
