ac868b7afa7783358ca7726d37b9560d6e285567
Samuel.Ssenyonga <samuel.senyonga@baisoftglobal.co.ug>
2026-03-18 22:06:23 +0300

    minor

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
